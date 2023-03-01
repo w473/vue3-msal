f1f80255bc1d4e5463b202b1f5f1c5daa88c1a3e
Jacek Glogosz <jacek.glogosz@gmail.com>
2023-03-01 22:18:28 +0100

    local build setup

--- lib/mixin.ts
+++ lib/mixin.ts
@@ -1,10 +1,12 @@
 export const mixin = {
     data: function() {
         return {
+            // @ts-ignore
             msal: (this.$msal) ? this.$msal.data : {}
         }
     },
     created: function() {
+        // @ts-ignore
         this.$watch('$msal.data', (value) => { this.msal = value; }, { deep: true });
     }
 };
